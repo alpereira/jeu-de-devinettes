8494607a77c56b24daf5ab2faad797dfb9b69eda
Alexandre Pereira <alexandrepereira.emploi@gmail.com>
2020-01-02 20:57:31 +0100

    add existing files

---
+++ jeu de devinettes/assets/js/index.js
@@ -0,0 +1,49 @@
+/*1. Le systeme genere un nombre aléatoire compris entre 1 et 10
+2. Le joueur a droit à 3 tentatives pour deviner ce nombre
+3. S'il trouve le nombre avant les 3 tentatives, il est déclaré gagnant
+4. Sinon il est déclaré perdant
+5. Il a la possibilité de toujours refaire une nouvelle partie*/
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+var num = Math.floor(Math.random() * 10) + 1;
+
+var NbEssais = 0;
+
+function Devine() {
+
+        var choisi = document.form1.devine1.value;
+
+        NbEssais++;
+
+        status = "Nombre d'essais : " + NbEssais;
+
+        if (choisi < num) {
+
+                window.alert("Non, le nombre est plus grand.");
+
+        }
+
+        if (choisi > num) {
+
+                window.alert("Non, le nombre est plus petit.");
+
+        }
+
+        if (choisi == num) {
+
+                window.alert("Correct ! Vous avez trouvé en " + NbEssais + " essais.");
+
+                location.reload();
+
+        }
+
+        if (NbEssais == 3) {
+
+                window.alert("Désolé, c'est fini. Le nombre correct était : " + num);
+
+                location.reload();
+
+        }
+
+}
